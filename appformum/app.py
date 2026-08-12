5460c6e615bc3aa1501f4cff980cb03ab124027c
phoebeplatelas-oss <phoebeplatelas@gmail.com>
2026-08-12 10:38:17 +0100

    Remove MAIL_PASSWORD from app configuration
    initial no p

--- appformum/app.py
+++ appformum/app.py
@@ -16,7 +16,7 @@ app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USERNAME"] = "phoebeplatelas@gmail.com"
-app.config["MAIL_PASSWORD"] = "lbxxfclsglrvlcvg"  
+app.config["MAIL_PASSWORD"] = ""  
 mail = Mail(app)
 
 jobstores = {
